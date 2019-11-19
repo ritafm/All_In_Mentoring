044bcef6e14b91023467b0efc0b9ced144340a2d
Dominic Jenkins <dominicjenkins@hotmail.com>
2019-11-18 21:16:45 -0800

    working local host connection

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
-const express = require('express');
-const PORT = process.env.PORT || 8080;
+const express = require("express");
+// const mongoose = require("mongoose");
+// const routes = require("./routes");
 const app = express();
+const PORT = process.env.PORT || 3001;
+// Configure body parsing for AJAX requests
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Serve up static assets
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+// Add routes, both API and view
+// app.use(routes);
+// Connect to the Mongo DB
+// if we are working in a production enviorment this connects to our mongo db with moongoose and if it doesn't exist it will create it.
 
-// We neeed to build the folder tree 
-// Paste everything besides server.js into the client folder
 
-app.listen(PORT, () => {
-  console.log(`listening on port ${PORT}!`);
-});
+// Start the API server
+app.listen(PORT, () =>
+  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`)
+);
