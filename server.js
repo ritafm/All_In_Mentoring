5c42000825aacd0c190039ff0c96cf862b3ad3f2
Dominic Jenkins <dominicjenkins@hotmail.com>
2019-11-18 21:06:13 -0800

    setting up server files

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const PORT = process.env.PORT || 8080;
 const app = express();
 
+// We neeed to build the folder tree 
+// Paste everything besides server.js into the client folder
+
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}!`);
 });
