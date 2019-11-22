454aebdeb63165fcaeb295faaddd5fa76eab943b
Rita Manley <manley.rita@gmail.com>
2019-11-21 19:48:56 -0800

    added yarn

---
+++ client/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Nav from "./components/Nav";
+import Index from "./src/components/StickyLayout/index"
+
+function App() {
+  return (
+    <div>
+      <Nav />
+      <Index />
+
+    </div>
+  );
+}
+
+export default App;
