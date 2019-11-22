8f07e4b97539c3a34e89e2849afd1828b8c57949
Rita Manley <manley.rita@gmail.com>
2019-11-21 21:00:15 -0800

    fixed folder structure

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import StickyLayout from "./components/StickyLayout"
+
+function App() {
+  return (
+    <div>
+      <StickyLayout />
+
+    </div>
+  );
+}
+
+export default App;
--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 const express = require('express');
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3001;
 const app = express();
 const router = express.Router();
 
 // Routes
 
 // Home Page
-router.get('/', function (request, response) { 
+// router.get('/', function (request, response) { 
 
- });
+//  });
 
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}!`);
