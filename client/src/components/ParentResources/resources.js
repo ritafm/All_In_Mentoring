e684dcd574b2e64262bd78cb01f62d0e52292c7a
Rita Manley <manley.rita@gmail.com>
2019-12-04 21:55:16 -0800

    added pdf icons

--- client/src/components/ParentResources/resources.js
+++ client/src/components/ParentResources/resources.js
@@ -4,17 +4,14 @@ import { Item } from 'semantic-ui-react'
 const ResourceProps = () => (
   <Item.Group>
     <Item>
+      <Item.Content>
+        <Item.Header><a className="file pdf icon" data-content="Code of Conduct" href="/assets/pdf/codeOfConduct.pdf" target = "blank">Code of Conduct</a></Item.Header>
+        <i className="massive file pdf icon"></i>
+        </Item.Content>
 
       <Item.Content>
-        <Item.Header><a href="/assets/pdf/codeOfConduct.pdf" target = "blank">Code of Conduct</a></Item.Header>
-      </Item.Content>
-    </Item>
-
-    <Item>
-
-      <Item.Content>
-        <Item.Header><a href="/assets/pdf/liability.pdf" target = "blank">Activity Waiver</a></Item.Header>
-      </Item.Content>
+      <Item.Header><a className="file pdf icon" data-content="Activity Waiver" href="/assets/pdf/liability.pdf" target = "blank">Activity Waiver</a></Item.Header>
+        <i className="massive file pdf icon"></i>      </Item.Content>
     </Item>
   </Item.Group>
 )
